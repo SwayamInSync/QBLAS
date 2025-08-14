4b81767953a3b043a11ab5349ce56c9c960455eb
swayaminsync <hawkempire007@gmail.com>
2025-08-14 22:14:22 +0530

    replacing old legacy benchmarking with google benchmarks

--- include/quadblas/algorithms/level1.hpp
+++ include/quadblas/algorithms/level1.hpp
@@ -145,6 +145,97 @@ namespace QuadBLAS
     return result;
   }
 
+  // Vectorized AXPY kernel for aligned data (y = alpha * x + y)
+  inline void axpy_kernel_vectorized(Sleef_quad alpha, Sleef_quad *x, Sleef_quad *y, size_t n)
+  {
+    const size_t vec_n = n / VECTOR_SIZE;
+    const size_t remainder = n % VECTOR_SIZE;
+
+    QuadVector alpha_vec(alpha);
+
+    // Vectorized loop
+    for (size_t i = 0; i < vec_n; ++i)
+    {
+      QuadVector x_vec = QuadVector::load(&x[i * VECTOR_SIZE]);
+      QuadVector y_vec = QuadVector::load(&y[i * VECTOR_SIZE]);
+      QuadVector result_vec = x_vec.fma(alpha_vec, y_vec); // alpha*x + y
+      result_vec.store(&y[i * VECTOR_SIZE]);
+    }
+
+    // Handle remainder
+    for (size_t i = vec_n * VECTOR_SIZE; i < n; ++i)
+    {
+      y[i] = Sleef_fmaq1_u05(alpha, x[i], y[i]);
+    }
+  }
+
+  // Parallel AXPY for large vectors
+  inline void axpy_parallel(Sleef_quad alpha, Sleef_quad *x, Sleef_quad *y, size_t n)
+  {
+    if (n < PARALLEL_THRESHOLD)
+    {
+      axpy_kernel_vectorized(alpha, x, y, n);
+      return;
+    }
+
+#ifdef _OPENMP
+    const int num_threads = get_num_threads();
+
+#pragma omp parallel
+    {
+      int tid = omp_get_thread_num();
+      size_t chunk_size = n / num_threads;
+      size_t start = tid * chunk_size;
+      size_t end = (tid == num_threads - 1) ? n : start + chunk_size;
+
+      if (start < end)
+      {
+        axpy_kernel_vectorized(alpha, &x[start], &y[start], end - start);
+      }
+    }
+#else
+    axpy_kernel_vectorized(alpha, x, y, n);
+#endif
+  }
+
+  // Main AXPY function with stride support
+  inline void axpy(size_t n, Sleef_quad alpha, Sleef_quad *x, size_t incx, Sleef_quad *y, size_t incy)
+  {
+    if (n == 0)
+      return;
+
+    // Fast path for unit strides
+    if (incx == 1 && incy == 1)
+    {
+      axpy_parallel(alpha, x, y, n);
+      return;
+    }
+
+    // Strided access
+    if (n >= PARALLEL_THRESHOLD)
+    {
+#ifdef _OPENMP
+#pragma omp parallel for
+      for (size_t i = 0; i < n; ++i)
+      {
+        y[i * incy] = Sleef_fmaq1_u05(alpha, x[i * incx], y[i * incy]);
+      }
+#else
+      for (size_t i = 0; i < n; ++i)
+      {
+        y[i * incy] = Sleef_fmaq1_u05(alpha, x[i * incx], y[i * incy]);
+      }
+#endif
+    }
+    else
+    {
+      for (size_t i = 0; i < n; ++i)
+      {
+        y[i * incy] = Sleef_fmaq1_u05(alpha, x[i * incx], y[i * incy]);
+      }
+    }
+  }
+
 } // namespace QuadBLAS
 
 #endif // QUADBLAS_ALGORITHMS_LEVEL1_HPP
